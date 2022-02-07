69b9dd0d8a57784e525653ea351f17b722eebaa6
FIDAP123 <fitradanaputra@gmail.com>
2022-02-08 02:24:06 +0700

    menambah file

--- Latihan16 Fungsi dengan return/latihan16.cpp
+++ Latihan16 Fungsi dengan return/latihan16.cpp
@@ -5,10 +5,25 @@
 
 #include<iostream>
 
-int tambah (int x)
+int kuadrat (int x)
+{
+    int y ;
+
+    y = x*x;
+    
+    return y;
+}
 
 int main(int argc, char const *argv[])
 {
+    int hasil;
+    int input;
     
+    std::cout << "masukkan nilai kuadrat = ";
+    std::cin >> input;
+    hasil = kuadrat(input);
+    
+    std::cout << "hasilnya adalah " << hasil << std::endl;
+
     return 0;
 }
