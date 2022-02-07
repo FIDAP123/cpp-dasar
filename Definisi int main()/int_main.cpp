da64c92d33d894d5c8a1bd4d1a86aec7344cbffd
FIDAP123 <fitradanaputra@gmail.com>
2022-02-07 23:30:41 +0700

    menambah data

--- Definisi int main()/int_main.cpp
+++ Definisi int main()/int_main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-int main(int argc, char** argv) //
+int main(int argc, char** argv) //bisa ditulis int main() atau int main(int argc, char const *argv[])
 {
     std::cout << "Have " << argc << " arguments:" << std::endl;
     for (int i = 0; i <argc; ++i) 
     {
-        std::cout << argv[i] << std::endl;
+        std::cout << argv[i] << std::endl;// ini menunjukkan alur program int main dengan argc sebagai jumlah argumen yang sudah ditulis di dalam argumen vektor atau argv, vektor hampir sama dengan char
     }
-    std::cin.get();
-    return 0;
+    
+    return 0;//mengembalikan nilai argc ke nol agar program dapat di eksekusi
 }
