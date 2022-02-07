0f32f7e28cf03ff430a6208418822e7aa039d8ef
FIDAP123 <fitradanaputra@gmail.com>
2022-02-07 22:04:39 +0700

    revisi data

--- Definisi int main()/int_main.cpp
+++ Definisi int main()/int_main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv) //
 {
     std::cout << "Have " << argc << " arguments:" << std::endl;
     for (int i = 0; i <argc; ++i) 
---
+++ Latihan16 Fungsi dengan return/latihan16.cpp
@@ -0,0 +1,11 @@
+/*
+    Latihan 16
+*/
+
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
