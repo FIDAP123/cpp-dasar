7e76f099bfdfeca9a564013248605dbc2a48731c
FIDAP123 <fitradanaputra@gmail.com>
2022-02-07 23:57:15 +0700

    tambahan

--- Definisi int main()/int_main.cpp
+++ Definisi int main()/int_main.cpp
@@ -1,3 +1,15 @@
+/*
+    Definisi int main() :
+    1.  int main () merupakan function yang berfungsi untuk menjalankan source code yang kita tulis
+    2.  Untuk C++ sendiri source code harus di buat dengan function berbeda dengan python yang bisa dijalankan tanpa 
+        function
+    3.  int main () sendiri merupakan function singkatan dari int main(int argc, char** argv) atau int main(int argc, 
+        char const *argv[]) tujuan dibuat singkat agar programer tidak harus menuliskan banyak, da sebenarnya lebih bagus tidak menulis singkatanya agar mengerti alurnya, dan yang namanya function tidak boleh kosong jadi kita tidak menyingkatnya justru lebih benar
+    4.  int main(int argc, char const *argv[]) atau int main(int argc, char** argv) merupakan gerbang pembuka dan return 
+        0 merupakan gerbang penutup karena argc atau arguments count sendiri merupakan jumlah argumen yang dijalankan dengan berbentuk tipe data integer dan argv atau arguments vector yang merupakan isi dari arguments yang dijalankan itu atau isi dari argc return 0 sendiri artinya mengembalikan nilai int argc tadi menjadi 0 supaya program berhenti
+    5.  Bila program kita error maka return akan berubah menjadi lebih dari 0, angka itu berasal dari int argc yang kita 
+        tulis functionnya di int main, dengan begitu program akan berhenti dan akan timbul peringatan error dari sistem, karena program kita tidak bisa selesai, dan semua hal itu berlangsung di compiling karena untuk c++ sendiri merupakan bahasa compiler 
+*/
 #include <iostream>
 
 int main(int argc, char** argv) //bisa ditulis int main() atau int main(int argc, char const *argv[])
