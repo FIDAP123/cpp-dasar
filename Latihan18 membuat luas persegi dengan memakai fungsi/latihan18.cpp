52e6ba946e2937de5c16f654f669fccfb4f6a961
FIDAP123 <fitradanaputra@gmail.com>
2022-02-09 02:02:25 +0700

    menambah latihan 18

---
+++ Latihan18 membuat luas persegi dengan memakai fungsi/latihan18.cpp
@@ -0,0 +1,12 @@
+/*
+    latihan18 membuat luas persegi dengan memanfaatkan fungsi return dan void
+    1.  m
+*/
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "halo" << std::endl;
+    
+    return 0;
+}
