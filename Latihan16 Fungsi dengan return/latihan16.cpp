f3406c955b2270386d1c7530457113d6551c03a3
FIDAP123 <fitradanaputra@gmail.com>
2022-02-08 17:59:20 +0700

    tambah bahan belajar latihan 16

--- Latihan16 Fungsi dengan return/latihan16.cpp
+++ Latihan16 Fungsi dengan return/latihan16.cpp
@@ -14,16 +14,38 @@ int kuadrat (int x)
     return y;
 }
 
+int tambah (int a, int b)
+{
+    int c;
+    c = a + b;
+    return c;
+}
+
 int main(int argc, char const *argv[])
 {
     int hasil;
-    int input;
+    int input1;
+    int input2;
     
-    std::cout << "masukkan nilai kuadrat = ";
-    std::cin >> input;
-    hasil = kuadrat(input);
-    
-    std::cout << "hasilnya adalah " << hasil << std::endl;
+    std::cout << "masukkan nilai kuadrat1 = ";
+    std::cin >> input1;
+    hasil = kuadrat(input1);
+    std::cout << "hasil fungsi kuadrat(x) dalam var hasil = " << hasil << std::endl;
 
+    std::cout << "masukkan nilai kuadrat2 = ";
+    std::cin >> input2;
+    std::cout << "hasil fungsi kuadrat(x) tanpa var hasil = " << kuadrat(input2) << std::endl;
+    
+    std::cout << "masukkan nilai tambah huruf 1 = ";
+    std::cin >> input1;
+    std::cin >> input2;
+    hasil = tambah(input1,input2);
+    std::cout << "nilai tambah dengan var = " << hasil;
+
+    std::cout << "masukkan nilai tambah huruf 1 = ";
+    std::cin >> input1;
+    std::cin >> input2;
+    std::cout << "nilai tambah dengan var = " << tambah(input1,input2);
+    
     return 0;
 }
