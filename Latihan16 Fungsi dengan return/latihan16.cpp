41b23a582076c32c8ad962b9ae92891533c4aa3e
FIDAP123 <fitradanaputra@gmail.com>
2022-02-08 19:13:23 +0700

    perbaikan

--- Latihan16 Fungsi dengan return/latihan16.cpp
+++ Latihan16 Fungsi dengan return/latihan16.cpp
@@ -1,6 +1,8 @@
 /*
     Latihan 16 Fugsi dengan return :
-
+    1.  membuat fungsi return yang hampir sama seperti fungsi int main (), fungsi return atau fungsi kembalian adalah   
+        fungsi yang akan mengembalikan nilainya conntoh dalam matematika f(x)=x*x maka sama saja y=x*x dalam hal ini yang menjadi kembaliannya yaitu nilai y yang merupakan hasil dari x*x
+    2.  Dalam fungsi di C++ selalu ada 
 */
 
 #include<iostream>
@@ -34,18 +36,20 @@ int main(int argc, char const *argv[])
 
     std::cout << "masukkan nilai kuadrat2 = ";
     std::cin >> input2;
-    std::cout << "hasil fungsi kuadrat(x) tanpa var hasil = " << kuadrat(input2) << std::endl;
+    std::cout << "hasil fungsi kuadrat(x) tanpa var hasil = " << kuadrat(input2) << std::endl << std::endl;
     
     std::cout << "masukkan nilai tambah huruf 1 = ";
     std::cin >> input1;
+    std::cout << "masukkan nilai tambah huruf 2 = ";
     std::cin >> input2;
     hasil = tambah(input1,input2);
-    std::cout << "nilai tambah dengan var = " << hasil;
+    std::cout << "nilai tambah dengan var = " << hasil << std::endl;
 
     std::cout << "masukkan nilai tambah huruf 1 = ";
     std::cin >> input1;
+    std::cout << "masukkan nilai tambah huruf 2 = ";
     std::cin >> input2;
-    std::cout << "nilai tambah dengan var = " << tambah(input1,input2);
+    std::cout << "nilai tambah dengan var = " << tambah(input1,input2) << std::endl;
     
     return 0;
 }
