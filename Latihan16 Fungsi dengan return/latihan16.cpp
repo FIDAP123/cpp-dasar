28039fdae646a30aaa0fc639000a2da290432a68
FIDAP123 <fitradanaputra@gmail.com>
2022-02-09 01:38:07 +0700

    menambahkan

--- Latihan16 Fungsi dengan return/latihan16.cpp
+++ Latihan16 Fungsi dengan return/latihan16.cpp
@@ -1,8 +1,14 @@
 /*
     Latihan 16 Fugsi dengan return :
-    1.  membuat fungsi return yang hampir sama seperti fungsi int main (), fungsi return atau fungsi kembalian adalah   
-        fungsi yang akan mengembalikan fungsi itu sendiri sehingga fungsi dapat tampil di konsol dengan perantara variable lain
-    2.  function harus ada deklarasi sebagai penentu variable awal. 
+    1.  membuat fungsi return yang hampir sama seperti fungsi int main (), 
+        fungsi return atau fungsi kembalian adalah fungsi yang akan mengembalikan fungsi itu sendiri sehingga fungsi dapat tampil di konsol dengan perantara variable lain.
+    2.  Void di dalam fungsi void itu merupakan deklarasi.
+    4.  function dengan return harus ada deklarasi sebagai penentu variable awal.
+    5.  penulisan pada fungsi return di fungsi main harus menyertakan cout 
+        agar dapat tampil di console.
+    6.  fungsi void jika berada di fungsinya maka 
+        harus wajib menyertakan cout agar bisa tampil di console atau terminal.
+    7.  Fungsi void tidak bisa di 
 */
 
 #include<iostream>
@@ -10,9 +16,7 @@
 int kuadrat (int x)
 {
     int y ;
-
-    y = x*x;
-    
+    y=x*x;
     return y;
 }
 
@@ -20,20 +24,14 @@ int tambah (int a, int b)
 {
     int c;
     c = a + b;
+    std::cout << "nilai tambah = " << c << std::endl;
     return c;
 }
 
-void tampilkan(int input1)
-{
-    std::cout << "menampilkan void = ";
-    std::cout << input1 << std::endl;
-}
 
 int main(int argc, char const *argv[])
 {
-    int hasil;
-    int input1;
-    int input2;
+    int hasil, input1, input2;
     
     std::cout << "masukkan nilai kuadrat1 = ";
     std::cin >> input1;
@@ -42,22 +40,20 @@ int main(int argc, char const *argv[])
 
     std::cout << "masukkan nilai kuadrat2 = ";
     std::cin >> input2;
-    std::cout << "hasil fungsi kuadrat(x) tanpa var hasil = " << kuadrat(input2) << std::endl << std::endl;
+    std::cout << "hasil fungsi kuadrat(x) tanpa var hasil = " << kuadrat(input2) << std::endl;
     
     std::cout << "masukkan nilai tambah huruf 1 = ";
     std::cin >> input1;
     std::cout << "masukkan nilai tambah huruf 2 = ";
     std::cin >> input2;
-    hasil = tambah(input1,input2);
-    std::cout << "nilai tambah dengan var = " << hasil << std::endl;
+    //hasil = tambah(input1,input2);//untuk inisialisasi variable disini jika kita menuliskan seperti ini dan variable fungsi yang sudah kita simpan dalam hal ini yaitu pada variable hasil maka fungsi akan di tampilkan 2 kali, yang pertama ditampilkan menurut perintah yang ada di dalamnya yaitu std::cout << "nilai tambah = " << c << std::endl; , setelah itu yang kedua akan ditampilkan angkanya saja yaitu hasil dari penjumlahan variable ini yaitu 9, karena variable ini bertipe integer maka hasilnya ada angkanya, dan peulisan kita sepertinya kuran benar,
+    tambah(input1,input2);// karena penulisan inisialisasi diatas gagal maka kita tidak bisa menuliskan cout lagi, karena kita sudah memakai cout pada fungsi returnnya,
 
     std::cout << "masukkan nilai tambah huruf 1 = ";
     std::cin >> input1;
     std::cout << "masukkan nilai tambah huruf 2 = ";
     std::cin >> input2;
-    std::cout << "nilai tambah dengan var = " << tambah(input1,input2) << std::endl << std::endl;
-
-    tampilkan(hasil);
+    tambah(input1,input2);
 
     return 0;
 }
---
+++ Latihan17 Fungsi Void/latihan17.cpp
@@ -0,0 +1,36 @@
+/*
+    Latihan 17 Fungsi Void atau Fungsi tanpa kembali atau tanpa return :
+    1.  fungsi void jika berada di fungsinya maka 
+        harus wajib menyertakan cout agar bisa tampil di console atau terminal.
+    2.  Fungsi void tidak bisa di 
+*/
+#include<iostream>
+
+void tampilkan(int c)
+{
+    std::cout << "menampilkan void = ";
+    std::cout << c*c << std::endl; 
+}
+
+void tampilkan(int a, int b)
+{
+    std::cout << "menampilkan void = ";
+    std::cout << a+b << std::endl; 
+}
+
+int main(int argc, char const *argv[])
+{
+    int input1, input2, hasil;
+    std::cout << "masukkan nilai kuadrat1 = ";
+    std::cin >> input1;
+    //hasil = tampilkan(input1); // jika menulis seperti maka akan terjadi error karena fungsi void tidak mempunyai tipe data.
+    tampilkan(input1);// di fungsi void yang ada di fungsi int() kita tidak bisa menambahkan cout karena fungsi void tidak punya tipe data.
+
+    std::cout << "masukkan nilai tambah1 = ";
+    std::cin >> input1;
+    std::cout << "masukkan nilai tambah2 = ";
+    std::cin >> input2;
+    tampilkan(input1,input2);
+    
+    return 0;
+}
