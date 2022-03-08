4d69f5ac9037b8e93e4dde3ef4230a74b5bab526
FIDAP123 <fitradanaputra@gmail.com>
2022-03-09 00:30:11 +0700

    pusing

--- blm sls Latihan24 Membuat rekursif faktorial&fibonacci/latihan24.cpp
+++ blm sls Latihan24 Membuat rekursif faktorial&fibonacci/latihan24.cpp
@@ -3,6 +3,26 @@
 */
 #include<iostream>
 
+int faktorial(int a);
+void p_faktorial(int a);
+
+int fibonacci (int a);
+void p_fibonacci(int a);
+
+int main(int argc, char const *argv[])
+{
+    int a;
+
+    std::cout << "masukkan nilai faktorial = ";
+    std::cin >> a;
+    std::cout << "\n";
+
+    p_faktorial(a);
+    p_fibonacci(a);
+    
+    return 0;
+}
+
 int faktorial(int a)
 {
     if (a==1)
@@ -23,15 +43,29 @@ void p_faktorial(int a)
     std::cout << a << "!= " << faktorial(a) << std::endl;
 }
 
-int main(int argc, char const *argv[])
+int fibonacci (int a)
 {
-    int a;
-
-    std::cout << "masukkan nilai faktorial = ";
-    std::cin >> a;
-    std::cout << "\n";
-
-    p_faktorial(a);
+    int b,c,d;
     
-    return 0;
+    b=0;
+    c=1;
+    if (a==1)
+    {
+        std::cout << d << "," ;
+        return d;
+    }
+    else
+    {  
+        d= b+c;
+        std::cout << d << ",";
+        d= b + c + fibonacci(a-1);
+        b= d;
+        c = b + c;
+        return d;
+    }
+    
+}
+void p_fibonacci(int a)
+{
+    std::cout << "fibonacci "<< a << " = " << "0,1,"<< fibonacci(a) << std::endl;
 }
