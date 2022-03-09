3cc76b4e839ef8202470cb34ea13449cbabf49d5
FIDAP123 <fitradanaputra@gmail.com>
2022-03-10 03:08:08 +0700

    pointers

--- Latihan25 Pointer/latihan25.cpp
+++ Latihan25 Pointer/latihan25.cpp
@@ -1,3 +1,10 @@
+/*
+    Latihan 25 Pointer :
+    1.  pointer berguna mengatur memori pada tipe data tertentu
+    2.  perintah &a adalah addresing atau pengalamatan memori, misal &a maksudnya    
+        kita ingin memanggil addressing dari a
+    3.  pointer dilambangkan *
+*/
 #include<iostream>
 
 int main(int argc, char const *argv[])
@@ -5,11 +12,11 @@ int main(int argc, char const *argv[])
     /*--------------------------Nilai a = 3--------------------------*/
     int a;
     a=3;
-    int *aPtr;
-    aPtr = &a;
+    int *aPtr;//* adalah pointer yang menandakan kalau ini berarti kita akan menyimpan address memory kita disini
+    aPtr = &a;// bisa juga kita menuliskan langsung int *aPtr = &a
     int *kosongPtr = nullptr;
-    int *isi_nullPtr = nullptr;
-    isi_nullPtr = &a;
+    int *isi_nullPtr = nullptr;// null disini maksudnya kita membuat pointer dengan address kosong
+    isi_nullPtr = &a;// ini maksudnya kita akan mengisi pointer yang alamatnya kosong degan alamat dari a
     
     std::cout<< "nilai a = " << a << std::endl;
     std::cout<< "memori nilai a = " << aPtr << std::endl;
