aa6c492ad7baf231bfb8bd2f88b5cded7e2c7a9f
FIDAP123 <fitradanaputra@gmail.com>
2022-03-21 23:12:23 +0700

    edit

--- Latihan25 Pointer dan Reference/latihan25.cpp
+++ Latihan25 Pointer dan Reference/latihan25.cpp
@@ -1,9 +1,12 @@
 /*
     Latihan 25 Pointer :
     1. a.   pointer berguna mengatur memori pada tipe data tertentu
-       b.   perintah &a adalah addresing atau pengalamatan memori, misal &a maksudnya kita ingin memanggil addressing dari a
+       b.   perintah &a adalah addresing atau pengalamatan memori, misal &a maksudnya kita ingin memanggil addressing 
+            dari a
        c.   pointer dilambangkan *
-       d.   pointer tidak cocok dijadikan inputan karena jika dijadikan inputan maka poiter harus berisi pengalamatan memorinya bukan valuenya
+       d.   pointer tidak cocok dijadikan inputan karena jika dijadikan inputan maka poiter harus berisi pengalamatan 
+            memorinya bukan valuenya
+       e.   Pointer adalah variable yang hanya bisa menyimpan memori
     2. a.   reference adalah pengelamatan memori yang sama-sama disimpan oleh 2 variable
 */
 #include<iostream>
@@ -23,7 +26,7 @@ int main(int argc, char const *argv[])
     std::cout<< "nilai a = " << a << std::endl;
     std::cout<< "memori nilai a = " << aPtr << std::endl;
     std::cout<< "memori kosong atau null pointer = " << kosongPtr << std::endl;
-    std::cout<< "memori kosong atau null pointer yag diisi = " << isi_nullPtr << std::endl;
+    std::cout<< "memori kosong atau null pointer yag diisi memori a = " << isi_nullPtr << std::endl;
     std::cout<< "memori nilai dari aPtr = " << *aPtr << std::endl;
 
     /*-------------------Nilai a = 90------------------------------*/
@@ -45,14 +48,15 @@ int main(int argc, char const *argv[])
     std::cout << "-----------Reference-----------" << std::endl;
     //int c; untuk reference kita tidak boleh menulis deklarasi pertama kali dengan variable
     int &c = a;//untuk deklarasinya kita tulisan kan seperti ini, ini artinya kita akan menaruh variable a ke alamat dari c atau alamat c = a
-    std::cout << "memori nilai c = " << c << std::endl << std::endl;
-    std::cout<< "memori nilai dari c = " << &c << std::endl;
-    c = 2;// dengan ini maka a dan c akan berubah valuenya dan alamatnya juga sama berbeda dengan kita meuliskan lagsung a=c maka sistem akan membuat 2 memori sekaligus, tujuan dari reference adalah supaya komputer lebih ringan untuk menjalankan program karena alamat atau memorinya sama
-    std::cout << c << std::endl;
+    std::cout << "nilai c = " << c << std::endl;
+    std::cout<< "memori nilai c = " << &c << std::endl << std::endl;
     
-    std::cout << "memori nilai c = " << c << std::endl << std::endl;
-    std::cout<< "memori nilai dari c = " << &c << std::endl;
-    std::cout << "memori nilai a = " << a << std::endl << std::endl;
+    c = 2;// dengan ini maka a dan c akan berubah valuenya dan alamatnya juga sama berbeda dengan kita meuliskan lagsung a=c maka sistem akan membuat 2 memori sekaligus, tujuan dari reference adalah supaya komputer lebih ringan untuk menjalankan program karena alamat atau memorinya sama
+    
+    std::cout << "nilai c = " << c << std::endl;
+    std::cout<< "memori nilai c = " << &c << std::endl << std::endl;
+
+    std::cout << "memori nilai a = " << a << std::endl;
     std::cout<< "memori nilai dari a = " << &a << std::endl;
     // disini nilai value atau memori dari a dan b akan sama karena reference
     return 0;
