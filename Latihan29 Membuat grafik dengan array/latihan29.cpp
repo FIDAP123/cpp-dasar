2dee0fcf9a280f19f240b72e07c9fb07e85e1f7b
FIDAP123 <fitradanaputra@gmail.com>
2022-03-25 21:30:13 +0700

    edit

--- Latihan29 Membuat grafik dengan array/latihan29.cpp
+++ Latihan29 Membuat grafik dengan array/latihan29.cpp
@@ -3,24 +3,35 @@
 
 int main(int argc, char const *argv[])
 {
-    std::array<long, 6> tahun;
+    std::array<double, 5> tahun;
     
-    std::cout << "Data Siswa Kelas A dari tahun 2000 s/d 2022 :\n" << std::endl;
+    std::cout << "Data Rata-Rata Jumlah Siswa Kelas A dari tahun 2001 s/d 2022 :\n" << std::endl;
 
+    
     for (int i = 0; i < tahun.size(); i++)
     {
+        int tahun_2 = 2005;
         std::cout << "Silahkan masukkan data di tahun ";
         if (i == 0)
         {
-            std::cout << "2000 s/d 2005 = " ;
+            std::cout << "2001 s/d 2005 = " ;
             std::cin >> tahun[i];
+        }
+        else if (i == 4)
+        {
+            std::cout << "2021 s/d 2022 = " ;
+            std::cin >> tahun[i];
+            
         }
         
         else
         {
-            std::cout << 2005+i << " s/d " << 2000+i ;
+            int tahun_1 = tahun_2 +i;
+            tahun_2 = tahun_1 + 4;
+            std::cout << tahun_1 << " s/d " << tahun_2 ;
             std::cin >> tahun[i];
         }
+
     }
     
 
