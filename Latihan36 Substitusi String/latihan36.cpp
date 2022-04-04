47f66c4bf5d9572814c2f60d9d5d481290025ffb
FIDAP123 <fitradanaputra@gmail.com>
2022-04-04 22:35:55 +0700

    edit

--- Latihan36 Substitusi String/latihan36.cpp
+++ Latihan36 Substitusi String/latihan36.cpp
@@ -8,6 +8,23 @@
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    std::string kalimat_1 = "sarimin makan mi sarimi";
+    std::string kalimat_2("kalau tidak bobok digigit nyamuk");
+
+    std::cout << "Sebelum kalimat 1 dan 2 di swap : " << std::endl;
+    std::cout << "kalimat 1 = " << kalimat_1 << std::endl;
+    std::cout << "kalimat 2 = " << kalimat_2 << std::endl;
+    
+    std::cout << std::endl;
+    
+    kalimat_1.swap(kalimat_2);
+    std::cout << "Sesudah kalimat 1 dan 2 di swap" << std::endl;
+    std::cout << "kalimat 1 = " << kalimat_2 << std::endl;
+    std::cout << "kalimat 2 = " << kalimat_1 << std::endl;
+
+    std::cout << std::endl;
+
+    
+
     return 0;
 }
