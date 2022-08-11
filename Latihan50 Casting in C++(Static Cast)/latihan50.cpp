67dc0bca9ebc80cb96929b1823ba0d357a881744
FIDAP123 <fitradanaputra@gmail.com>
2022-08-12 01:42:00 +0700

    rencana selesai static cast hari ini

--- Latihan50 Casting in C++(Static Cast)/latihan50.cpp
+++ Latihan50 Casting in C++(Static Cast)/latihan50.cpp
@@ -1,33 +1,36 @@
 /*
 	Latihan 48 Casting in C static cast :
-	1. 	Salah satu fungsi static cast adalah implicit casting, implicit casting adalah casting tipe data dengan default dari compilernya dan tentunya casting didalam memori lebih aman, namun hasil yang 
+	1. 	Salah satu fungsi static cast adalah implicit casting, implicit casting adalah casting tipe data dengan default 
+		dari compilernya dan tentunya casting didalam memori lebih aman, namun hasil yang 
 */
 
 #include <iostream>
 
 int main(int argc, char const *argv[])
 {   
+	std::cout << "1. Static cast bisa digunakan untuk implicit cast dan eksplicit cast" << std::endl;
+
 	float desimal_1 = 9.54;
 	int bulat_2 = 29;
-
-	std::cout << "-------------implicit cast biasa------------\n" << std::endl;
+	
+	std::cout << "a. Implicit Cast Biasa\n" << std::endl;
 
 	bulat_2 = desimal_1 + 0.9;//ditambah dengan float supaya bisa mengecek tipe data dari variable hasilnya, karena c++ tipe data float tidak ada penanda desimal atau koma nol(.0)
 	std::cout << "desimal ke bulat = " << bulat_2 << std::endl;//jika casting biasa untuk tipe data dari value ditentukan berdasarkan tipe data dari variablenya
 	
-	bulat_2 = 29;
+	bulat_2 = 29;//inisialisasi disini berfungsi agar valuenya kembali ke awal
 	desimal_1 = bulat_2 + 0.9;
 	std::cout << "bulat ke desimal = " << desimal_1 << std::endl;	
 
 	std::cout  << std::endl;
 	
-	std::cout << "---------implicit cast dengan static cast--------\n" << std::endl;
+	std::cout << "b. Implicit Cast Dengan Static Cast\n" << std::endl;
 
 	desimal_1 = 9.54;
 	bulat_2 = static_cast<int>(desimal_1+0.9);
 	std::cout << "desimal ke integer (bulat isi)= " << bulat_2 << std::endl;
 	desimal_1 = 9.54;
-	bulat_2 = static_cast<float>(desimal_1)+0.9;
+	bulat_2 = static_cast<float>(desimal_1)+ 0.9;
 	std::cout << "desimal ke float (bulat isi)= " << bulat_2 << std::endl;
 
 	bulat_2 = 29;
@@ -36,6 +39,12 @@ int main(int argc, char const *argv[])
 	bulat_2 = 29;
 	desimal_1 = static_cast<int>(bulat_2 + 0.9);//hasilnya tetap tipe float tetapi karena kita memakai static cast yang berguna untuk casting tipe data maka hasilnya akan berupa integer tetapi tipe data variable akhirnya masihlah float dan untuk c++ sendiri tidak ada koma nol(.0) maka valuenya seperti tipe data integer namun tipe data dan value dari variable akhirnya tetap float
 	std::cout << "bulat ke integer = " << desimal_1 << std::endl;
+
+	std::cout  << std::endl;
+
+	std::cout << "2. Static cast bisa digunakan untuk conversion constructor & conversion operator OOP/PBO di C++" << std::endl;
+
+	
 
 	return 0;
 }
